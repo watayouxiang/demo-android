6f7bcf2b42d4baf2485f48c878e151db2048e06b
TaoWang <watayouxiang@qq.com>
2017-10-11 21:03:02 +0800

    add RetrofitDemo

---
+++ RetrofitDemo/app/src/main/java/com/cniao5/retrofitdemo/BaseResult.java
@@ -0,0 +1,36 @@
+package com.cniao5.retrofitdemo;
+
+/**
+ * Created by Ivan on 2016/10/7.
+ */
+
+public class BaseResult {
+
+    private int success;
+    private String message;
+    private int user_id;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(int success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
